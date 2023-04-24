5a48ad992125865d6c0966417c101a9234ed05a1
Nelson Torres <ntorres381@gmail.com>
2023-04-24 15:39:24 -0700

    returns matching results using array filter() method

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution here
+
+// function findMatching(drivers, string){
+//     const newDrivers = [];
+
+//     for(const driver of drivers){
+//         if(driver.toLowerCase() === string.toLowerCase() ){
+         
+//            newDrivers.push(driver)
+//         } 
+
+//     }
+//    return newDrivers
+// }
+
+
+const findMatching = (drivers, string) => drivers.filter(driver => driver.toLowerCase() === string.toLowerCase())
+ 
+
+
+ const fuzzyMatch = (drivers, string) => drivers.filter((driver) => driver.toLowerCase().indexOf(string.toLowerCase()) === 0)
+
+
+
+const matchName = (drivers, string) =>  drivers.filter( (driver) => driver.name === string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
